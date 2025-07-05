71ee010cebddd940d521d1d6aeabf8945d940a43
jradak01 <jradak98@gmail.com>
2025-07-05 21:18:18 +0200

    feat(auth): added model for authentication

---
+++ src/models/auth_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+# Request model for login
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+# Response model for login
+class LoginResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    username: str
+
